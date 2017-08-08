9db68565447287f9a3ec07ddfcbd279622009af4
gommog <gom.mog@mail.ee>
2017-08-08 23:26:19 +0300

    some more experiment to understand of pointer and operations this them

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,38 +10,35 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            //Point p1 = new Point(1,3,'*');
-            //p1.Draw();
-
-            //Point p2 = new Point(4,5,'#');
-            //p2.Draw();
-
-            //Console.ReadLine();
-            //*******************************************************************************
-
-            int x = 1;
-            Func1(x);
-            Console.WriteLine("Func1.x = " + x);
-            x = 1;
-            Func2(x);
-            Console.WriteLine("Func2.x = " + x);
-            x = 1;
-            Func3(x);
-            Console.WriteLine("Func3.x = " + x);
-
             Point p1 = new Point(1, 3, '*');
-            Move(p1, 10, 10);
-            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
-
+            p1.Draw();
             Point p2 = new Point(4, 5, '#');
-            p1 = p2;
-            p2.x = 8;
-            p2.y = 8;
-            Console.WriteLine("p1 = p2. p1.x = " + p1.x + "p1.y = " + p1.y + ", p2.x = " + p2.x + ", p2.y = " + p2.y);
+            p2.Draw();
 
-            p1 = new Point(1, 1, '*');
-            Update(p1);
-            Console.WriteLine("Call Update. p1.x = " + p1.x + ", p1.y = " + p1.y);
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("   p1  ---> " + p1.x + "  " + p1.y);
+            Console.WriteLine("   p2  ---> " + p2.x + "  " + p2.y);
+
+            p1 = p2; // p2 = p1; <-- у автора
+
+            Console.WriteLine(" ");
+            Console.WriteLine("   p1=p2 p1 ---> " + p1.x + "  " + p1.y);
+            Console.WriteLine("   p1=p2 p2 ---> " + p2.x + "  " + p2.y);
+
+            p2.x = 8; // p1.x = 8; для экперемента можно и эти координаты
+            p2.y = 8; // p2.y = 6; меняет координаты обоих Point
+
+            Console.WriteLine(" ");
+            Console.WriteLine("   new p2(8,8) p1 ---> " + p1.x + "  " + p1.y);
+            Console.WriteLine("   new p2(8,8) p2 ---> " + p2.x + "  " + p2.y);
+
+            p1.x = 3; // Не важно какие координаты менять х или у 1-ой или 2-ой Point 
+            p2.y = 5; // меняется всё т.к. адрес тот же
+
+            Console.WriteLine(" ");
+            Console.WriteLine("   new p1.x = 3 p1 ---> " + p1.x + "  " + p1.y);
+            Console.WriteLine("   new p2.y = 5 p2 ---> " + p2.x + "  " + p2.y);
 
             Console.ReadLine();
         }
@@ -70,7 +67,7 @@ namespace Snake
 
         public static void Update (Point p)
         {
-
+            //p = new Point();
         }
     }
 }
