e04cba8d8b437d95b58401bd73ebbb27833e8b04
Gom Mog <gom.mog@mail.ee>
2017-08-14 16:29:50 +0300

    I am trying to do class by myself

---
+++ Snake/HorizonalLine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizonalLine //: Figure
+    {
+        public int x_begin;
+        public int x_end;
+        public int y;
+        public char sym;
+
+        public HorizonalLine(int _x_begin, int _x_end, int _y, char _sym)
+        {
+            x_begin = _x_begin;
+            x_end = _x_end;
+            y = _y;
+            sym = _sym;
+        
+        }
+    }
+}
